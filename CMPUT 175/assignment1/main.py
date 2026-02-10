3e6c36d57c637bddd0357457a868f0a7d046ad09
Barret108 <barretappiah@gmail.com>
2026-02-10 16:29:11 -0700

    assignment 1, section A

--- CMPUT 175/assignment1/main.py
+++ CMPUT 175/assignment1/main.py
@@ -1,16 +1,31 @@
 
 def countries_deficit():
+    deficit = []
+
+    # Gets country import export data
     with open('country.txt', 'r') as file:
         countries = file.read().splitlines()
-        for country in countries:
-            country = country.join(',')
-    print(countries)
 
-def table_maker_a(countries):
+        # Removes titles
+        countries.pop(0)
+
+        # Appends each countries deficit
+        for country in countries:
+            country = country.split(',')
+            deficit.append([float(country[2]) - float(country[3]), country[1]])
+    
+    # Sorts by greatest deficit
+    deficit.sort(key=lambda row: row[0], reverse=True)
+    return deficit
+              
+def table_maker_a(deficit):
     titleOne = 'Country'
     titleTwo = 'Trade Deficit (Billions USD)'
     sizeOne = 34
     sizeTwo = 28
+
+    top_five = deficit[:5]
+    top_five_deficits = {country: value for value, country in top_five}
 
     # Formatting
     title = '| ' + titleOne + (' ' * (sizeOne - len(titleOne) - 1)) + ' | ' + titleTwo + (' ' * (sizeTwo - len(titleTwo))) + ' |'
@@ -21,15 +36,15 @@ def table_maker_a(countries):
     print(title)
     print(bar)
 
-    for country in countries:
-        print('| ' + country + (' ' * (sizeOne - len(country) - 1)) + ' | $' + (' ' * (sizeTwo - len(str(countries[country])) - 1)) + str(countries[country]) + ' |')
+    for country in top_five_deficits:
+        print('| ' + country + (' ' * (sizeOne - len(country) - 1)) + ' | $' + (' ' * (sizeTwo - len(str(top_five_deficits[country])) - 1)) + str(top_five_deficits[country]) + ' |')
     
     print(bar)
 
 def main():
     # Section A: countries with highest trade deficits
-    countries = {'Belgium':2 , 'Canada':3}
-    countries = countries_deficit()
-    table_maker_a(countries)
+
+    deficit = countries_deficit()
+    table_maker_a(deficit)
 
 main()
